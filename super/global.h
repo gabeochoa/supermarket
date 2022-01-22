94217f6ab69fc9626a6ffaaf4b514f5ba6fffef2
gabeochoa <gabeochoa@fb.com>
2022-01-21 22:32:10 -0500

    move commands into global.h

--- super/global.h
+++ super/global.h
@@ -1,7 +1,15 @@
 
 #pragma once
 
+#include <any>
+#include <sstream>
+
 #include "../vendor/supermarket-engine/engine/camera.h"
+#include "../vendor/supermarket-engine/engine/commands.h"
+#include "../vendor/supermarket-engine/engine/globals.h"
+#include "../vendor/supermarket-engine/engine/edit.h"
+
+#include "menu.h"
 
 constexpr int WIN_W = 1920;
 constexpr int WIN_H = 1080;
@@ -14,4 +22,43 @@ constexpr bool IS_DEBUG = true;
 static std::shared_ptr<OrthoCameraController> menuCameraController;
 static std::shared_ptr<OrthoCameraController> uiTestCameraController;
 static std::shared_ptr<OrthoCameraController> terminalCameraController;
+
+struct SetMenuCommand : Command<Menu> {
+    std::vector<std::any> convert(const std::vector<std::string>& tokens) {
+        // Need to convert to T* and T
+        std::vector<std::any> out;
+        if (tokens.size() != 1) {
+            this->msg = fmt::format("Invalid number of parameters {} wanted {}",
+                                    tokens.size(), 1);
+            return out;
+        }
+        out.push_back(GLOBALS.get_ptr<Menu>("menu_state"));
+        int val = Deserializer<int>(tokens[0]);
+        Menu::State state = static_cast<Menu::State>(val);
+        out.push_back(state);
+        return out;
+    }
+
+    std::string operator()(const std::vector<std::string>& params) {
+        auto values = this->convert(params);
+        if (!values.empty()) {
+            Menu* m = std::any_cast<Menu*>(values[0]);
+            Menu::State val = std::any_cast<Menu::State>(values[1]);
+            m->state = val;
+            this->msg = fmt::format("{} is now {}", params[0], val);
+        }
+        return this->msg;
+    }
+};
+
+
+inline void add_globals() {
+    //
+    Menu::get();  // need this to create the menu state in the first place
+
+    GLOBALS.set("menu_state", menu.get());
+    EDITOR_COMMANDS.registerCommand("set_menu_state", SetMenuCommand(),
+                                    "Change Menu:: state; set_menu_state <id>");
+}
+
 
--- super/main.cpp
+++ super/main.cpp
@@ -6,7 +6,6 @@
 // #define SUPER_ENGINE_PROFILING_DISABLED
 
 #include "../vendor/supermarket-engine/engine/app.h"
-#include "../vendor/supermarket-engine/engine/edit.h"
 #include "../vendor/supermarket-engine/engine/file.h"
 #include "../vendor/supermarket-engine/engine/input.h"
 #include "../vendor/supermarket-engine/engine/pch.hpp"
@@ -28,43 +27,6 @@
 #include "tests.h"
 #include "uitest.h"
 
-struct SetMenuCommand : Command<Menu> {
-    std::vector<std::any> convert(const std::vector<std::string>& tokens) {
-        // Need to convert to T* and T
-        std::vector<std::any> out;
-        if (tokens.size() != 1) {
-            this->msg = fmt::format("Invalid number of parameters {} wanted {}",
-                                    tokens.size(), 1);
-            return out;
-        }
-        out.push_back(GLOBALS.get_ptr<Menu>("menu_state"));
-        int val = Deserializer<int>(tokens[0]);
-        Menu::State state = static_cast<Menu::State>(val);
-        out.push_back(state);
-        return out;
-    }
-
-    std::string operator()(const std::vector<std::string>& params) {
-        auto values = this->convert(params);
-        if (!values.empty()) {
-            Menu* m = std::any_cast<Menu*>(values[0]);
-            Menu::State val = std::any_cast<Menu::State>(values[1]);
-            m->state = val;
-            this->msg = fmt::format("{} is now {}", params[0], val);
-        }
-        return this->msg;
-    }
-};
-
-void add_globals() {
-    //
-    Menu::get();  // need this to create the menu state in the first place
-
-    GLOBALS.set("menu_state", menu.get());
-    EDITOR_COMMANDS.registerCommand("set_menu_state", SetMenuCommand(),
-                                    "Change Menu:: state; set_menu_state <id>");
-}
-
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
@@ -76,7 +38,6 @@ int main(int argc, char** argv) {
     backward::SignalHandling sh;
 
     all_tests();
-
     add_globals();
 
     App::create({
@@ -121,9 +82,9 @@ int main(int argc, char** argv) {
     Layer* uitest = new UITestLayer();
     App::get().pushLayer(uitest);
 
-    Menu::get().state = Menu::State::Game;
+    // Menu::get().state = Menu::State::Game;
     // Menu::get().state = Menu::State::Root;
-    // Menu::get().state = Menu::State::UITest;
+    Menu::get().state = Menu::State::UITest;
 
     App::get().run();
 
